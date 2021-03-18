07602e2290f4c97e60fca7a35774b2dff0d2c08d
vlod <vladalex420@gmail.com>
2021-03-18 22:40:22 +0200

    test

---
+++ cmake-build-debug/thirdparty/gl2d/CMakeFiles/gl2d.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mihui/Dev/fantasyCraft/thirdparty/gl2d/src/gl2d.cpp" "/home/mihui/Dev/fantasyCraft/cmake-build-debug/thirdparty/gl2d/CMakeFiles/gl2d.dir/src/gl2d.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../thirdparty/gl2d/include"
+  "../thirdparty/glm"
+  "../thirdparty/glad/include"
+  "../thirdparty/stb_image/include"
+  "../thirdparty/stb_truetype/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/mihui/Dev/fantasyCraft/cmake-build-debug/thirdparty/glad/CMakeFiles/glad.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
